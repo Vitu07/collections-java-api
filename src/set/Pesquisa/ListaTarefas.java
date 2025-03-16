ccf43ea496feae9ef48c3bf40decf6ed9d90c977
Vitu07 <vitormorais20092004@gmail.com>
2025-03-15 22:48:18 -0300

    Enviando exercicio ListaTarefas Pesquisa set

---
+++ src/set/Pesquisa/ListaTarefas.java
@@ -0,0 +1,112 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefas {
+
+    private Set<Tarefa> taskList;
+
+    public ListaTarefas() {
+        this.taskList = new HashSet<>();
+    }
+
+    public void adcionarTarefa(String descricao){
+        taskList.add(new Tarefa(descricao));
+    }
+
+    public void  removerTarefa(String descricao){
+        Tarefa tarefaParaRemover = null;
+        for (Tarefa t : taskList){
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaParaRemover = t;
+                break;
+            }
+        }
+        taskList.remove(tarefaParaRemover);
+    }
+
+    public Set<Tarefa> obterTarefasConcluidas(){
+        Set<Tarefa>  tarefasConcluidas = new HashSet<>();
+        for (Tarefa t : taskList){
+            if (t.isTarefaConcluida()){
+                tarefasConcluidas.add(t);
+            }
+        }
+        return tarefasConcluidas;
+    }
+
+    public Set<Tarefa> obterTarefasPendentes(){
+        Set<Tarefa> tarefasPendentes = new HashSet<>();
+        for (Tarefa t : taskList){
+            if (!t.isTarefaConcluida()){
+                tarefasPendentes.add(t);
+            }
+        }
+        return tarefasPendentes;
+    }
+
+    public Tarefa marcarTarefaConcluida(String descricao){
+        Tarefa tarefaConcluida = null;
+        for (Tarefa t : taskList){
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                t.setTarefaConcluida(true);
+                tarefaConcluida = t;
+                break;
+            }
+        }
+        return tarefaConcluida;
+    }
+
+    public Tarefa marcarTarefaPendente(String descricao){
+        Tarefa tarefaPendente = null;
+        for (Tarefa t : taskList){
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaPendente = t;
+                break;
+            }
+        }
+        return tarefaPendente;
+    }
+
+    public void limparTarefas(){
+        Set<Tarefa> tarefasParaRemover = new HashSet<>(taskList);
+        taskList.removeAll(tarefasParaRemover);
+        System.out.println("Todas tarefas removidas!");
+    }
+
+    public void exibirTarefas(){
+        System.out.println("Lista de tarefas :" + taskList);
+    }
+
+    public void contarTarefas(){
+        System.out.println("Numero total de tarefas: " + taskList.size());
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+
+        listaTarefas.adcionarTarefa("Beber agua");
+        listaTarefas.adcionarTarefa("Jantar");
+        listaTarefas.adcionarTarefa("Limpar o quarto");
+        listaTarefas.adcionarTarefa("Ir para academia");
+        listaTarefas.adcionarTarefa("Correr");
+        listaTarefas.adcionarTarefa("Estudar Java");
+
+        listaTarefas.exibirTarefas();
+        listaTarefas.contarTarefas();
+
+        listaTarefas.marcarTarefaConcluida("Estudar Java");
+        listaTarefas.marcarTarefaConcluida("Correr");
+        listaTarefas.marcarTarefaConcluida("Limpar o quarto");
+        listaTarefas.marcarTarefaPendente("Beber agua");
+        listaTarefas.marcarTarefaPendente("Ir para academia");
+        listaTarefas.removerTarefa("Jantar");
+
+        System.out.println(listaTarefas.obterTarefasConcluidas());
+        System.out.println(listaTarefas.obterTarefasPendentes());
+        listaTarefas.limparTarefas();
+        listaTarefas.exibirTarefas();
+    }
+}
---
+++ src/set/Pesquisa/Tarefa.java
@@ -0,0 +1,44 @@
+package set.Pesquisa;
+
+import java.util.Objects;
+
+public class Tarefa {
+    private String descricao;
+    private boolean tarefaConcluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isTarefaConcluida() {
+        return tarefaConcluida;
+    }
+
+    public void setTarefaConcluida(boolean tarefaConcluida) {
+        this.tarefaConcluida = tarefaConcluida;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(getDescricao(), tarefa.getDescricao());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getDescricao());
+    }
+
+    @Override
+    public String toString() {
+        return "\nTarefa{" +
+                "descricao='" + descricao + '\'' +
+                ", tarefaConcluida=" + tarefaConcluida +
+                '}';
+    }
+}
